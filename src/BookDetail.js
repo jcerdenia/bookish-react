0fe453977c45f4af62958c4aa711122a7a961f58
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-04 14:13:37 +0800

    make BookDetail unit test pass

--- src/BookDetail.js
+++ src/BookDetail.js
@@ -3,9 +3,8 @@ import React from 'react';
 function BookDetail({ book }) {
   return (
     <div className='detail'>
-      <h2 className='book-title'>
-        {book.name}
-      </h2>
+      <h2 className='book-title'>{book.name}</h2>
+      <p className='book-description'>{book.description}</p>
     </div>
   );
 }
