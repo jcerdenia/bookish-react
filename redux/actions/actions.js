471b663a322e75ab0f6662021842e21466db5fdf
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-05 16:01:01 +0800

    update dependencies, reorder folders

--- redux/actions/actions.js
+++
@@ -1,7 +0,0 @@
-// An action creator:
-// Will create an action and bind with events from user interaction.
-export const setSearchTerm = (term) => {
-  // Actions have a 'type' prop that signal the type of action performed;
-  // other than that, the structure of an action object is up to us to define.
-  return { type: 'SET_SEARCH_TERM', term }
-}
